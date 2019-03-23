768e084d986742962fad17a6ac8b41dd75566e48
Richard Gordon <richgordon95@gmail.com>
2019-03-23 18:05:59 -0400

    initial project structure

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class CoursesPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      course: {
+        title: ""
+      }
+    }
+  }
+
+  handleChange(event) {
+    const course = { ...this.state.course, title: event.target.value};
+    this.setState({ course: course });
+    this.setState({ course }); //same as above line (shorthand)
+
+  }
+
+  render() {
+    return (
+      <form>
+          <h2>Courses</h2>
+          <h3>Add Course</h3>
+          <input type="text"
+            onChange={this.handleChange}
+            value={this.state.course.title} />
+          <input type="submit" value="Save" />
+      </form>
+    );
+  }
+}
+
+export default CoursesPage;
