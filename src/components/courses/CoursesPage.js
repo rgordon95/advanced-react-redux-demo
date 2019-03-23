65390afcc07128350cfe631e4804f43262f2b21f
Richard Gordon <richgordon95@gmail.com>
2019-03-23 18:22:54 -0400

    basic structure of redux

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 class CoursesPage extends React.Component {
-
     state = {
       course: {
         title: ""
@@ -16,9 +15,14 @@ class CoursesPage extends React.Component {
 
   };
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert(this.state.course.title);
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
           <h2>Courses</h2>
           <h3>Add Course</h3>
           <input type="text"
