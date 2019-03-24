fef2db8b5eef2f120a00b63935e8c7c090a4e1d3
Richard Gordon <richgordon95@gmail.com>
2019-03-24 13:19:27 -0400

    add first thunk middleware

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -23,7 +23,7 @@ module.exports = {
     https: false
   },
   plugins:  [
-    new webpack.DefinePLugin({
+    new webpack.DefinePlugin({
       "process.env.API_URL": JSON.stringify("http://localhost:3001")
   }),
     new HtmlWebpackPlugin({
