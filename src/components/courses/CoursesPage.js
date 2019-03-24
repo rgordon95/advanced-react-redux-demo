86113e6fa294458375685e9d306fb1bbccf568d5
Richard Gordon <richgordon95@gmail.com>
2019-03-24 14:24:02 -0400

    add author name redux flow

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import * as courseActions from '../../redux/actions/courseActions';
+import * as authorActions from '../../redux/actions/authorActions';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import CourseList from './CourseList';
@@ -10,6 +11,11 @@ class CoursesPage extends React.Component {
     this.props.actions.loadCourses()
     .catch(error => {
       alert("Loading courses failed.." + error);
+    });
+
+    this.props.actions.loadAuthors()
+    .catch(error => {
+      alert("Loading authors failed.." + error);
     });
   }
   render() {
@@ -29,13 +35,24 @@ CoursesPage.propTypes = {
 
 function mapStateToProps(state) {
   return {
-    courses: state.courses
+    courses: state.authors.length === 0
+    ? []
+    : state.courses.map(course => {
+      return {
+        ...course,
+        authorName: state.authors.find(a => a.id === course.authorId).name
+      };
+    }),
+    authors: state.authors
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators(courseActions, dispatch)
+    actions: {
+      loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+    }
   };
 }
 
