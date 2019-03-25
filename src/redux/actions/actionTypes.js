2e1c4aaafb194d6e9a789cb503587558dac35357
Richard Gordon <richgordon95@gmail.com>
2019-03-25 18:17:06 -0400

    delete course added

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -5,3 +5,10 @@ export const CREATE_COURSE_SUCCESS = "CREATE_COURSE_SUCCESS";
 export const UPDATE_COURSE_SUCCESS = "UPDATE_COURSE_SUCCESS";
 export const BEGIN_API_CALL = "BEGIN_API_CALL";
 export const API_CALL_ERROR = "API_CALL_ERROR"
+
+// By convention, actions that end in "_SUCCESS" are assumed to have been the result of
+// a completed API call. Since we are doing an optimistic delete, we are hiding loading state.
+// that is why this action does not have success in it.
+// if it had _SUCCESS suffix, our apiCallsInProgress counter would be decremented below zero.
+// because we are not incrementing the number of apiCallsInProgress when delte request begins.
+export const DELETE_COURSE_OPTIMISTIC = "DELETE_COURSE_OPTIMISTIC";
--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -14,6 +14,10 @@ export function updateCourseSuccess(course) {
   return { type: types.UPDATE_COURSE_SUCCESS, course };
 }
 
+export function deleteCourseOptimistic(course) {
+  return { type: types.DELETE_COURSE_OPTIMISTIC, course };
+}
+
 export function loadCourses() {
   return function (dispatch) {
     dispatch(beginApiCall());
@@ -45,4 +49,13 @@ export function saveCourse(course) {
       throw error;
     });
   };
+}
+
+export function deleteCourse(course) {
+  return function (dispatch) {
+    // Doing optimistic delete, so not dispatching begin/end api call
+    // actions, or apiCallError action since were not showing the laoding status for this.
+    dispatch(deleteCourseOptimistic(course));
+    return courseApi.deleteCourse(course.id);
+  }
 }
