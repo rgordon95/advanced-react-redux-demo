ec4918176952d6c2a4e80e5c30f4babd471304ab
Richard Gordon <richgordon95@gmail.com>
2019-03-24 20:23:03 -0400

    make courses editable as long as you dont nav directly to url

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -61,9 +61,17 @@ ManageCoursePage.propTypes = {
   history: PropTypes.object.isRequred
 };
 
-function mapStateToProps(state) {
+export function getCourseBySlug(courses, slug) {
+  return courses.find(course => course.slug === slug) || null;
+}
+
+function mapStateToProps(state, ownProps) {
+  const slug = ownProps.match.params.slug;
+  const course = slug && state.courses.length > 0
+  ? getCourseBySlug(state.courses, slug)
+  : newCourse;
   return {
-    course: newCourse,
+    course: course,
     courses: state.courses,
     authors: state.authors
   };
