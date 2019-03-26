958cde74678b38f51a2829bb8f406e9e5ddf7448
Richard Gordon <richgordon95@gmail.com>
2019-03-26 12:41:44 -0400

    set logic to select proper configureStore file depending on dev or bld

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,15 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from './reducers';
-import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
-import thunk from 'redux-thunk';
-
-export default function configureStore(initialState) {
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //add support for dev tools
-
-  return createStore(
-    rootReducer,
-    initialState,
-    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
-  );
+// Uses CommonJs below so we can dyamically import during build time.
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./configureStore.prod");
+} else {
+  module.exports = require("./configureStore.dev");
 }
