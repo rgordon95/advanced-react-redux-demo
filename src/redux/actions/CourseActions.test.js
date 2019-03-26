73c5a49a45d6bf1bca0ce2b9846c2391872500d0
Richard Gordon <richgordon95@gmail.com>
2019-03-26 12:23:09 -0400

    thunk test 1

--- src/redux/actions/CourseActions.test.js
+++ src/redux/actions/CourseActions.test.js
@@ -13,7 +13,25 @@ describe('Async Actions', () => {
   afterEach(() => {
     fetchMock.restore();
   });
-    
+
+    describe("Load Courses Thunk", () => {
+      it("should create BEGIN_API_CALL and LOAD_COURSES_SUCCESS when loading courses", () => {
+        fetchMock.mock("*", {
+          body: courses,
+          headers: { "content-type": "application/json" }
+        });
+
+        const expectedActions = [
+          { type: types.BEGIN_API_CALL },
+          { type: types.LOAD_COURSES_SUCCESS, courses }
+        ];
+
+        const store = mockStore({ courses: [] });
+        return store.dispatch(courseActions.loadCourses()).then(() => {
+          expect(store.getActions()).toEqual(expectedActions);
+        });
+      });
+    });
 });
 
 
