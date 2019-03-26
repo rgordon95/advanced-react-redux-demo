e063239411d5b4deb77029cfe4fb87420ade9705
Richard Gordon <richgordon95@gmail.com>
2019-03-26 12:31:09 -0400

    reducer test 2

--- src/redux/reducers/CourseReducer.test.js
+++ src/redux/reducers/CourseReducer.test.js
@@ -27,3 +27,25 @@ it('should add course when passed CREATE_COURSE_SUCCESS', () => {
   expect(newState[1].title).toEqual("B");
   expect(newState[2].title).toEqual("C");
 });
+
+it("should update course when passed UPDATE_COURSE_SUCCESS", () => {
+  // arrange
+  const initialState = [
+    { id: 1, title: "A" },
+    { id: 2, title: "B" },
+    { id: 3, title: "C" }
+  ];
+
+  const course = { id: 2, title: "New Title" };
+  const action = actions.updateCourseSuccess(course);
+
+  // act
+  const newState = courseReducer(initialState, action);
+  const updatedCourse = newState.find(a => a.id == course.id);
+  const untouchedCourse = newState.find(a => a.id == 1);
+
+  // assert
+  expect(updatedCourse.title).toEqual("New Title");
+  expect(untouchedCourse.title).toEqual("A");
+  expect(newState.length).toEqual(3);
+})
